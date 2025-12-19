ea9bf5494701d1717d8282f65896b82f18d79f4d
Radu <radupopa028@gmail.com>
2025-12-19 10:36:04 +0200

    chore: Update dependencies and remove redundant main tag from home page.

--- app/page.tsx
+++ app/page.tsx
@@ -9,19 +9,17 @@ export default function Home() {
   const headerHeight = useHeaderHeight()
 
   return (
-    <main>
-      <section
-        className="grid grid-rows-[1fr_auto]"
-        style={{
-          height: `calc(100dvh - ${headerHeight}px)`,
-          // Fallback for older browsers
-          minHeight: `calc(100vh - ${headerHeight}px)`,
-        }}
-      >
-        <HeroCarousel />
+    <section
+      className="grid grid-rows-[1fr_auto]"
+      style={{
+        height: `calc(100dvh - ${headerHeight}px)`,
+        // Fallback for older browsers
+        minHeight: `calc(100vh - ${headerHeight}px)`,
+      }}
+    >
+      <HeroCarousel />
 
-        <TrustedByCarousel />
-      </section>
-    </main>
+      <TrustedByCarousel />
+    </section>
   )
 }
